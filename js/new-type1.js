8f418dec75174382c2df3bb29458493d940810b8
iDash <idash3.b@gmail.com>
2018-01-02 23:55:25 -0600

    New Test Template and JS logic made.

--- js/new-type1.js
+++ js/new-type1.js
@@ -1,3 +1,85 @@
-$().ready(function(){
+var mainCanvas = document.getElementById("thumbnail");
+var ctx = mainCanvas.getContext("2d");
 
+$().ready(function(){
+	loadTest('static/ex-img/rec-ex.png')
+	$('#mainRefresh').hide()
+	function onFileSelect(buttonID, fileControl){
+		$(buttonID).on('change', ':file', function() {
+		  var input = $(this),
+		    numFiles = input.get(0).files ? input.get(0).files.length : 1,
+		    label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+		  if(numFiles != 0){
+	  		$(fileControl).html(label)
+		  }
+		});
+	}
+	$('#mainPreview')
+		.click(function(){
+			$('#previewCanvas')
+				.show()
+			$('#previewMenu')
+				.removeClass('push-md-4')
+			$('#mainRefresh').show()
+			$('#mainPreview').hide()
+		})
+		// Must change into a revursive way.
+		$('#btn-file').click(function(){
+			onFileSelect('#btn-file', '#file-control')
+		})
+		$('.btn-download').click(function(){
+			convertCanvasToImage(mainCanvas);
+		})
 })
+function loadTest(displayPictureUrl){
+	background(ctx, '#8BC34A', '#FF9800')
+	addTitle(ctx, 'Zac Efron', 'title');
+	addTitle(ctx, 'Amante de animales', 'subtitle');
+	addMainImage(ctx, displayPictureUrl);
+	addResultImage(ctx, "static/imgs/zac-animal.jpg");
+	var actualCanvas = convertCanvasToImage(mainCanvas);
+}
+function background(ctx, color1, color2){
+	// Create gradient
+	// var grd=ctx.createLinearGradient(0,0,200,0); Creates a linear gradient.
+	// var grd=ctx.createLinearGradient(0,0,0,200); Creates a up do down gradient.
+	var grd=ctx.createLinearGradient(0,0,200,200);
+	grd.addColorStop(0,color1);
+	grd.addColorStop(1,color2);
+	// Fill with gradient
+	ctx.fillStyle=grd;
+	ctx.fillRect(0,0,525,300);	
+}
+function addTitle(ctx, text, type){
+	// Add a function so that the center of the text goes to the center of the canvas.
+	if (type == 'title'){	
+		ctx.fillStyle='white';
+		ctx.font='800 30pt Roboto'
+		ctx.fillText(text,200,55)
+	}
+	else if (type == 'subtitle') {
+		ctx.fillStyle='#2196F3';
+		ctx.font='500 25pt Roboto'
+		ctx.fillText(text,100,100)
+	}
+}
+function addMainImage(ctx, imgUrl){
+	let img = new Image()
+	img.src = imgUrl;
+	img.crossOrigin = "Anonymous";
+	img.addEventListener("load", function(){
+		ctx.drawImage(img,50,110,180,180)
+	}, false)
+}
+function addResultImage(ctx, imgUrl){
+	let img = new Image()
+	img.src = imgUrl;
+	img.addEventListener("load", function(){
+		ctx.drawImage(img,270,110,180,180)
+	}, false)
+}
+function convertCanvasToImage(canvas){
+	let img = new Image()
+	img.src=canvas.toDataURL("image/png")
+	return img
+}
