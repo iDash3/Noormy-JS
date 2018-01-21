d80c8f86c1b92bbd0c0df5c6be4e3e2da4ab752f
iDash <idash3.b@gmail.com>
2018-01-20 20:55:46 -0600

    Desisgn changes to Brave and Index templates.

--- js/index-c.js
+++ js/index-c.js
@@ -10,15 +10,22 @@ function fbLoginStatus(){
               if (response.name != 'undefined'){
                 username = ' ' + response.name + ' ';
               }
-              $('#sht-div').html('Hey' + '<b class="amber"> ' + username + '</b>' + ', CUSTOM MESSAGE HERE')
+              let custom_msg = [
+                'perfect afternoon for your brain to relax, huh?',
+                'a certain prince sends his regards...',
+                'you are part of us now!',
+                'hope you enjoy the content!',
+                ]
+              let random_msg = custom_msg[Math.floor(Math.random() * custom_msg.length)];
+              $('#sht-div').html('Hey' + '<b class="amber"> ' + username + '</b>' + ', ' + random_msg);
             });
-            $('#fb-login-item').hide()
-            $('#fb-logout-item').show()
+            $('.fb-login').hide()
+            $('.fb-logout').show()
         } else if (response.status === 'not_authorized') {
             $('#sht-div').html('Authorize our app in order to use it. :)')
         } else {
           let connectSpan = $('<span>Not connected to Facebook.\
-            <a id="fb-blue"><b>Click here</b> to connect.</a></span>')
+            <div id="fb-blue"><b>Click here</b> to connect.</div></span>')
             $('#sht-div').html(connectSpan)
         }
     });
@@ -50,33 +57,32 @@ window.fbAsyncInit = function() {
        fjs.parentNode.insertBefore(js, fjs);
      }(document, 'script', 'facebook-jssdk'));
 
+$('.fb-login, #fb-blue')
+  .click(function(){
+    console.log('Facebook Login')
+    FB.login(function(response){
+      if (response.authResponse) {
+        console.log('Welcome!  Fetching your information.... ');
+        fbLoginStatus()
+        FB.api('/me', function(response) {
+          console.log('Good to see you, ' + response.name + '.');
+        });
+      } else {
+        console.log('User cancelled login or did not fully authorize.');
+      }
+    },{scope: 'public_profile,email'});
+})
+$('.fb-logout')
+  .click(function(){
+    console.log('Facebook Logout')
+    FB.logout(function(response) {
+      deleteCookie("fblo_" + fbAppId); // fblo_yourFBAppId. example: fblo_444499089231295
+      fbLoginStatus();
+    });
+})
+
 $().ready(function(){
-  $('#fb-logout-item').hide()
-  $('#fb-login-item, #fb-blue')
-    .click(function(){
-      console.log('Facebook Login')
-      FB.login(function(response){
-        if (response.authResponse) {
-          console.log('Welcome!  Fetching your information.... ');
-          fbLoginStatus()
-          FB.api('/me', function(response) {
-            console.log('Good to see you, ' + response.name + '.');
-          });
-        } else {
-          console.log('User cancelled login or did not fully authorize.');
-        }
-      },{scope: 'public_profile,email'});
-  })
-    
-  $('#fb-logout-item')
-    .click(function(){
-      console.log('Facebook Logout')
-      FB.logout(function(response) {
-        deleteCookie("fblo_" + fbAppId); // fblo_yourFBAppId. example: fblo_444499089231295
-        fbLoginStatus();
-      });
-  })
-  
+  $('.fb-logout').hide()
   window.onscroll = function() {
     var header = document.getElementById("header-container");
     var sticky = 80
