678e7c19fb26e27c6fb3e1a90c3a3359ab8e1dbe
iDash <idash3.b@gmail.com>
2018-01-21 17:57:14 -0600

    Bug on FB login text.

--- js/index-c.js
+++ js/index-c.js
@@ -1,32 +1,31 @@
+var custom_msg = [
+  'perfect afternoon for your brain to relax, huh?',
+  'a certain prince sends his regards...',
+  'you are part of us now!',
+  'hope you enjoy the content!',
+]
 function fbLoginStatus(){
     FB.getLoginStatus(function(response) {
-    console.log(response.status)
-    console.log(document.cookie)
+      console.log(response.status)
+      console.log(document.cookie)
         if (response.status === 'connected') {
-            var uid = response.authResponse.userID;
-            var accessToken = response.authResponse.accessToken;
-            FB.api('/me', function(response) {
-              let username = '';
-              if (response.name != 'undefined'){
-                username = ' ' + response.name + ' ';
-              }
-              let custom_msg = [
-                'perfect afternoon for your brain to relax, huh?',
-                'a certain prince sends his regards...',
-                'you are part of us now!',
-                'hope you enjoy the content!',
-                ]
-              let random_msg = custom_msg[Math.floor(Math.random() * custom_msg.length)];
-              $('#sht-div').html('Hey' + '<b class="amber"> ' + username + '</b>' + ', ' + random_msg);
-            });
-            $('.fb-login').hide()
-            $('.fb-logout').show()
+          var uid = response.authResponse.userID;
+          var accessToken = response.authResponse.accessToken;
+          FB.api('/me', function(response) {
+            let username = '';
+            if (response.name != 'undefined'){
+              username = ' ' + response.name + ' ';
+            }
+            let random_msg = custom_msg[Math.floor(Math.random() * custom_msg.length)];
+            $('#sht-div').html('Hey' + '<b class="amber"> ' + username + '</b>' + ', ' + random_msg);
+          });
+          $('.fb-login').hide()
+          $('.fb-logout').show()
         } else if (response.status === 'not_authorized') {
             $('#sht-div').html('Authorize our app in order to use it. :)')
         } else {
-          let connectSpan = $('<span>Not connected to Facebook.\
-            <div id="fb-blue"><b>Click here</b> to connect.</div></span>')
-            $('#sht-div').html(connectSpan)
+          $('.fb-login').show()
+          $('.fb-logout').hide()
         }
     });
 }
@@ -57,32 +56,36 @@ window.fbAsyncInit = function() {
        fjs.parentNode.insertBefore(js, fjs);
      }(document, 'script', 'facebook-jssdk'));
 
-$('.fb-login, #fb-blue')
-  .click(function(){
-    console.log('Facebook Login')
-    FB.login(function(response){
-      if (response.authResponse) {
-        console.log('Welcome!  Fetching your information.... ');
-        fbLoginStatus()
-        FB.api('/me', function(response) {
-          console.log('Good to see you, ' + response.name + '.');
-        });
-      } else {
-        console.log('User cancelled login or did not fully authorize.');
-      }
-    },{scope: 'public_profile,email'});
-})
-$('.fb-logout')
-  .click(function(){
-    console.log('Facebook Logout')
-    FB.logout(function(response) {
-      deleteCookie("fblo_" + fbAppId); // fblo_yourFBAppId. example: fblo_444499089231295
-      fbLoginStatus();
-    });
-})
-
 $().ready(function(){
   $('.fb-logout').hide()
+
+  $('#sht-div').html($('<span>Not connected to Facebook.\
+  <span id="fb-blue">Click here to connect.</span></span>'))
+  
+  $('.fb-login, #fb-blue')
+    .click(function(){
+      console.log('Facebook Login')
+      FB.login(function(response){
+        if (response.authResponse) {
+          console.log('Welcome!  Fetching your information.... ');
+          fbLoginStatus()
+          FB.api('/me', function(response) {
+            console.log('Good to see you, ' + response.name + '.');
+          });
+        } else {
+          console.log('User cancelled login or did not fully authorize.');
+        }
+      },{scope: 'public_profile,email'});
+  })
+  $('.fb-logout')
+    .click(function(){
+      console.log('Facebook Logout')
+      FB.logout(function(response) {
+        deleteCookie("fblo_" + fbAppId);
+        fbLoginStatus();
+      });
+  })
+
   window.onscroll = function() {
     var header = document.getElementById("header-container");
     var sticky = 80
